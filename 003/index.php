4112e5a94be70831cc4c17d4c9d5514e45adc813
FABIO EMANUELL PEREIRA MILAGRES <fabioemanuellmilagres@gmail.com>
2026-03-31 14:45:50 -0300

    up trabalho 03

---
+++ 003/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+	<meta charset="UTF-8" />
+
+	<title>Integração</title>
+
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 70%;
+			margin-top: 10px;
+		}
+
+		table,
+		th,
+		td {
+			border: 1px solid black;
+		}
+
+		th,
+		td {
+			padding: 8px;
+			text-align: left;
+		}
+
+		th {
+			background-color: #ddd;
+		}
+	</style>
+
+</head>
+
+<body>
+
+	<?php
+
+	require_once 'conexao.php';
+
+	$conn = new Connection("localhost", "atividade_03", "root", "root");
+	$pdoConn = $conn->getConnection();
+
+	?>
+
+	<h1>Exercício de Integração (frontend, backend e banco de dados)</h1>
+
+	<form id="f" method="post" action="https://postman-echo.com/post">
+
+		<label for="nome"> Nome: </label>
+		<input type="text" id="nome" name="nome" size="40" maxlength="40" />
+
+		<br />
+
+		Tipo de Pessoa:
+
+		<input type="radio" id="pfisica" name="tipo" value="fisica" />
+		<label for="pfisica"> Física </label>
+
+		<input type="radio" id="pjuridica" name="tipo" value="juridica" />
+		<label for="pjuridica"> Jurídica </label>
+
+		<br />
+
+		<label for="cpf_cnpj"> CPF/CNPJ: </label>
+		<input type="text" id="cpf_cnpj" name="cpf_cnpj" />
+
+		<br />
+
+		<input type="submit" id="enviar" value="   Enviar   " />
+
+		<br />
+
+		<input type="reset" id="limpar" value="   Limpar   " />
+
+		<br />
+
+	</form>
+
+	<!-- NOVA TABELA -->
+	<h2>Dados Cadastrados</h2>
+	<table>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nome</th>
+				<th>Tipo de Pessoa</th>
+				<th>CPF/CNPJ</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+
+			$sql = "SELECT * FROM pessoas;";
+			$result = $pdoConn->query($sql);
+
+			if ($result->rowCount() > 0) {
+				while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+					echo "<tr>";
+					echo "<td>" . $row["id"] . "</td>";
+					echo "<td>" . $row["nome"] . "</td>";
+					echo "<td>" . ($row["tipo"] == "F" ? "Física" : "Juridica") . "</td>";
+					echo "<td>" . $row["cpf_cnpj"] . "</td>";
+					echo "</tr>";
+				}
+			} else {
+				echo "Nenhum registro encontrado.";
+			}
+
+			?>
+		</tbody>
+	</table>
+
+
+</body>
+
+</html>
